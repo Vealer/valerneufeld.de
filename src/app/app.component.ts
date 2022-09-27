d3dec2d1a1e6a08c26e0023d64fbf210010797ab
Valer Neufeld <valerneufeld@outlook.de>
2022-09-27 18:41:49 +0200

    add images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'valerneufeld';
+  images = ['macbook.jpg', 'office.jpg', 'table.jpg'];
+  currentImage = 0;
+  showImage = true;
 
 
-  
+ngOnInit(){
+  this.updateImage();
+}
+
+  updateImage() {
+    
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
